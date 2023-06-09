1cd79a075aff0e7ac2499429e1e0c45b6d5c9abe
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-09 16:21:20 +0200

    clean-up in App.tsx

--- backend/src/main/java/de/neuefische/backend/controller/HelloWorldController.java
+++ backend/src/main/java/de/neuefische/backend/controller/HelloWorldController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("api")
 public class HelloWorldController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World! Hello fly.io!";
+        return "Hello World!";
     }
 }
