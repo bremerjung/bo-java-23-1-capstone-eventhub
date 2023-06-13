b044f17d8c78fd9d6168522b31f504053b641189
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-13 13:52:26 +0200

    added event model

--- backend/src/main/java/de/neuefische/backend/model/Event.java
+++ backend/src/main/java/de/neuefische/backend/model/Event.java
@@ -1,10 +1,22 @@
 package de.neuefische.backend.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Event {
     private String id;
+    private String title;
+    private String description;
+    private Instant start;
+    private Instant end;
+    private String location;
+    private String category;
+    private String creator;
+    private String status;
 }
