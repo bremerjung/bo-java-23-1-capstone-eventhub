d580806f9cffdaa9fcc3d0ea3c40d2337b678cf5
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-14 09:18:16 +0200

    added event view

---
+++ frontend/src/components/EventGallery.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {EventModel} from "../model/EventModel";
+import EventCard from "./EventCard";
+import './EventGallery.css';
+
+type Props ={
+    events:EventModel[]
+}
+
+function EventGallery(props:Props) {
+
+    return (
+        <div className="event-gallery-gallery">
+            {props.events.map((currentEvent:EventModel)=>{
+                return <EventCard key={currentEvent.id} event={currentEvent}></EventCard>
+            })
+            }
+        </div>
+    );
+}
+
+export default EventGallery;
