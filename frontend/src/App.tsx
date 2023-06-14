f6aa18a21ab4313de1612d4f3a67631d24e1d073
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-14 09:26:00 +0200

    added route and link to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import useEvent from "./hooks/useEvent";
+import {Link, Route, Routes} from "react-router-dom";
+import EventGallery from "./components/EventGallery";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Event Hub - Die digitale Litfaßsäule</h1>
-      </header>
-    </div>
-  );
+
+    const {getAllEvents, events} = useEvent();
+
+    useEffect(() => {getAllEvents()}, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Event Hub - Die digitale Litfaßsäule</h1>
+            </header>
+            <main>
+                <Link to={'/gallery'}>Zu den Events</Link>
+            </main>
+            <Routes>
+                <Route path="/gallery" element={<EventGallery events={events}/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
