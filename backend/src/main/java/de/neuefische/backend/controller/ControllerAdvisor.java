5d96a533d4ca62ce4faee3f382b2c9f4f2334168
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-14 09:01:01 +0200

    added ControllerAdvisor

---
+++ backend/src/main/java/de/neuefische/backend/controller/ControllerAdvisor.java
@@ -0,0 +1,16 @@
+package de.neuefische.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ControllerAdvisor {
+    @ExceptionHandler(NoSuchElementException.class)
+    private ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
