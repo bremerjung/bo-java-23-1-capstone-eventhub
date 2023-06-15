b88b8dc95e94d4f0e4af8ef9dee5d13342a0f4c8
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-15 20:06:50 +0200

    extended custom user hook by error handling

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -1,26 +1,51 @@
 import axios from "axios";
 import {useState} from "react";
 import {User} from "../model/User";
+import {toast} from 'react-toastify';
 
 export default function useUser() {
 
     const [user, setUser] = useState<User>()
 
     function register(username: string, password: string) {
-        return axios.post("/api/user/register", { username, password })
-            .then((response) => setUser(response.data));
+        return axios.post("/api/user/register", {username, password})
+            .then((response) => {
+                setUser(response.data)
+                toast.success("Registration successful");
+            })
+            .catch((error) => {
+                const message =
+                    error?.response?.data || "Registration failed";
+                toast.error(message);
+            });
     }
 
     function login(username: string, password: string) {
         return axios.post("/api/user/login", undefined, {auth: {username, password}})
-            .then((response) => setUser(response.data))
+            .then((response) => {
+                setUser(response.data)
+                toast.success("Login successful");
+            })
+            .catch((error) => {
+                const message =
+                    "Login failed" + " " + error?.response?.data?.message;
+                toast.error(message);
+            });
     }
 
     function logout() {
         return axios.post("/api/user/logout")
-            .then(() => setUser(undefined));
+            .then(() => {
+                setUser(undefined)
+                toast.success("Logout successful");
+            })
+            .catch((error) => {
+                const message =
+                    "Logout failed" + " " + error?.response?.data?.message;
+                toast.error(message);
+            });
     }
 
-    return {register,login, logout, user};
+    return {register, login, logout, user};
 
 }
