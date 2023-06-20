bb56ad3950d0b92de125ff7fb45fa90a1564dd51
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-20 18:11:30 +0200

    added filter option to event gallery view

--- frontend/src/components/EventGallery.tsx
+++ frontend/src/components/EventGallery.tsx
@@ -2,19 +2,40 @@ import React from 'react';
 import {EventModel} from "../model/EventModel";
 import EventCard from "./EventCard";
 import './EventGallery.css';
+import {User} from "../model/User";
 
-type Props ={
-    events:EventModel[]
+type Props = {
+    user: User,
+    events: EventModel[],
+    getAllEvents: () => void,
+    getEventsByCategory: (categories: string[]) => void
 }
 
-function EventGallery(props:Props) {
+function EventGallery(props: Props) {
+
+    function onAllEventsClickHandler() {
+        console.log("All events clicked");
+        props.getAllEvents();
+    }
+
+    function onMyEventsClickHandler() {
+        console.log("My events clicked");
+        props.getEventsByCategory(props.user.preferredCategories);
+    }
 
     return (
-        <div className="event-gallery">
-            {props.events.map((currentEvent:EventModel)=>{
-                return <EventCard key={currentEvent.id} event={currentEvent}></EventCard>
-            })
-            }
+        <div>
+            <div>
+                <button onClick={onAllEventsClickHandler}>All events</button>
+                <button onClick={onMyEventsClickHandler}>My events</button>
+            </div>
+            <div className="event-gallery">
+
+                {props.events.map((currentEvent: EventModel) => {
+                    return <EventCard key={currentEvent.id} event={currentEvent}></EventCard>
+                })
+                }
+            </div>
         </div>
     );
 }
