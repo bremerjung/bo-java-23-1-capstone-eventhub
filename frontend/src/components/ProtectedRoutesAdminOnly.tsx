6bae32d26952783d537127efc57e0deb42701937
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-29 13:52:48 +0200

    adjusted protected routes

--- frontend/src/components/ProtectedRoutesAdminOnly.tsx
+++ frontend/src/components/ProtectedRoutesAdminOnly.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import {Navigate, Outlet} from "react-router-dom";
-import {User} from "../model/User";
+import getStoredUser from "./utils/getStoredUser";
 
-type Props = {
-    user: User | undefined
-}
+function ProtectedRoutesAdminOnly() {
 
-function ProtectedRoutesAdminOnly(props: Props) {
-
-    const authenticated = props.user?.roles?.includes("admin");
-
+    const storedUser = getStoredUser();
+    const authenticated = storedUser?.roles?.includes("admin");
 
     return (
         authenticated ? <Outlet/> : <Navigate to={"/gallery"}/>
