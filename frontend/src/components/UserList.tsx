2942e6a4e934c366e2bf5bec712c54147b2ade8c
Oliver Hofrichter <oli.bremen@gmail.com>
2023-07-01 20:47:51 +0200

    integrated toastify for role update and display role in user list

--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -4,10 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBriefcase, faCheck, faUser, faUserShield} from "@fortawesome/free-solid-svg-icons";
 import {User} from "../model/User";
 import useUser from "../hooks/useUser";
-
-type Props = {
-    users: User[]
-}
+import {toast} from "react-toastify";
 
 function UserList() {
 
@@ -18,58 +15,60 @@ function UserList() {
             .catch((error) => {
                 console.log(error);
             });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     function onSetUserRoleHandler(user: User, role: string) {
         updateUserRole(user.username, role)
+            .then(() => toast.success(`Changed role of ${user.username} to ${role}`))
             .catch((error) => {
                 console.log(error);
             });
     }
 
     function getUsernameBeforeAt(email: string) {
-        const username = email.split('@')[0];
-        return username;
+        return email.split('@')[0];
     }
 
     return (
         <div>
             <Container>
                 <ListGroup style={{width: '100%'}}>
-                    {users.map((currentUser, index) => (
-                        <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center">
-                            <div style={{
+                    {users.map((currentUser) => (
+                        <ListGroup.Item key={getUsernameBeforeAt(currentUser.username)}
+                                        className="d-flex justify-content-between align-items-center">
+                            <div className="small-device-font" style={{
                                 flex: 1,
                                 overflow: 'hidden',
                                 textOverflow: 'ellipsis',
                                 whiteSpace: 'nowrap'
-                            }}>{getUsernameBeforeAt(currentUser.username)}</div>
+                            }}>{getUsernameBeforeAt(currentUser.username)}{' ('}{currentUser.roles[0]}{')'}</div>
                             <div className="buttons">
                                 <OverlayTrigger placement="top" overlay={<Tooltip>Set role User</Tooltip>}>
-                                    <Button size="sm" className="button"
+                                    <Button size="sm" className="button m-1"
                                             onClick={() => onSetUserRoleHandler(currentUser, 'user')}>
-                                        <FontAwesomeIcon icon={faUser}/>
+                                        <FontAwesomeIcon className="small-icon" icon={faUser}/>
                                     </Button>
                                 </OverlayTrigger>
                                 <OverlayTrigger placement="top" overlay={<Tooltip>Set role Editor</Tooltip>}>
-                                    <Button size="sm" className="button"
+                                    <Button size="sm" className="button m-1"
                                             onClick={() => onSetUserRoleHandler(currentUser, 'editor')}>
-                                        <FontAwesomeIcon icon={faCheck}/>
+                                        <FontAwesomeIcon className="small-icon" icon={faCheck}/>
                                     </Button>
                                 </OverlayTrigger>
                                 <OverlayTrigger placement="top" overlay={<Tooltip>Set role Organizer</Tooltip>}>
                                     <Button
                                         size="sm"
-                                        className="button"
+                                        className="button m-1"
                                         onClick={() => onSetUserRoleHandler(currentUser, 'organizer')}
                                     >
-                                        <FontAwesomeIcon icon={faBriefcase}/>
+                                        <FontAwesomeIcon className="small-icon" icon={faBriefcase}/>
                                     </Button>
                                 </OverlayTrigger>
                                 <OverlayTrigger placement="top" overlay={<Tooltip>Set role Administrator</Tooltip>}>
                                     <Button size="sm" className="button"
                                             onClick={() => onSetUserRoleHandler(currentUser, 'admin')}>
-                                        <FontAwesomeIcon icon={faUserShield}/>
+                                        <FontAwesomeIcon className="small-icon" icon={faUserShield}/>
                                     </Button>
                                 </OverlayTrigger>
                             </div>
